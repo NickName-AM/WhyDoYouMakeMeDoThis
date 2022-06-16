5545fe98b72c7dce13662ddafeba55b8dc78910f
Vin.AM <75691501+NickName-AM@users.noreply.github.com>
2022-06-16 09:12:50 +0000

    Add files via upload

---
+++ sp_op.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	char c;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	printf("\nEnter a character: ");
+	scanf("%c", &c);
+
+	printf("Number: %d\n", n);
+	printf("Char: %c\n", c);
+}
